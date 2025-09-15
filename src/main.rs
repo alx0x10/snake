2030aef620a00eb27991adb8f98bf136514466d8
alx0x10 <fiotti.alexandre@gmail.com>
2025-09-15 21:24:59 +0200

    feat: print smiley

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,89 @@ mod fmt;
 
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
+use crate::{fmt::unwrap, image::IMG_SIG};
+
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::Timer;
-use fmt::info;
+use embassy_nrf::{gpio::{AnyPin, Level, Output, OutputDrive}, Peri};
+use embassy_time::{Duration, Ticker};
+use image::Image;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut row1 = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
-    let mut _col1 = Output::new(p.P0_28, Level::Low, OutputDrive::Standard);
+    
+    let rows = [
+        p.P0_21.into(),
+        p.P0_22.into(),
+        p.P0_15.into(),
+        p.P0_24.into(),
+        p.P0_19.into(),
+        ];
+        
+    let cols = [
+        p.P0_28.into(),
+        p.P0_11.into(),
+        p.P0_31.into(),
+        p.P1_05.into(),
+        p.P0_30.into(),
+    ];
+            
+    const HAPPY: Image = [
+        [0,1,0,1,0],
+        [0,1,0,1,0],
+        [0,0,0,0,0],
+        [1,0,0,0,1],
+        [0,1,1,1,0],
+    ];
+
+    unwrap!(spawner.spawn(display_task(rows, cols)));
+    
+    IMG_SIG.signal(HAPPY);
+}
+
+#[embassy_executor::task]
+async fn display_task(r_pins: [Peri<'static, AnyPin>; image::ROWS], c_pins: [Peri<'static, AnyPin>; image::COLS]) {
+    let mut r_pins = r_pins.map(|r| Output::new(r, Level::Low, OutputDrive::Standard));
+    let mut c_pins = c_pins.map(|c| Output::new(c, Level::High, OutputDrive::Standard));
+    
+    let mut img = image::IMG_SIG.wait().await;
+
+    let mut ticker = Ticker::every(Duration::from_hz(60 * img.len() as u64));
 
     loop {
-        info!("Hello, World!");
-        row1.set_high();
-        Timer::after_millis(500).await;
-        row1.set_low();
-        Timer::after_millis(500).await;
+        for (r_pin, r_img) in r_pins.iter_mut().zip(img) {
+            r_pin.set_high();
+
+            c_pins
+                .iter_mut()
+                .zip(r_img)
+                .filter(|(_, c_img)| *c_img != 0)
+                .for_each(|(c_pin, _)| c_pin.set_low());
+
+            ticker.next().await;
+
+            r_pin.set_low();
+            c_pins.iter_mut().for_each(|c_pin| c_pin.set_high());
+
+            if let Some(new_img) = IMG_SIG.try_take() {
+                img = new_img;
+
+                break
+            }
+        }
     }
+}
+
+mod image {
+    use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
+
+    pub const ROWS: usize = 5;
+    pub const COLS: usize = 5;
+
+    pub type Image = [[u8; COLS]; ROWS];
+
+    pub static IMG_SIG: Signal<CriticalSectionRawMutex, Image> = Signal::new();    
 }
